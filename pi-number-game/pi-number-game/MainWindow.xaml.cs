7b4a3b07e518ca389b908a65e14d0a6059211110
Liza Snizhko <snizhko.liza@gmail.com>
2022-08-16 11:40:57 -0700

    Realization of StopWatctEvent class

--- pi-number-game/pi-number-game/MainWindow.xaml.cs
+++ pi-number-game/pi-number-game/MainWindow.xaml.cs
@@ -1,4 +1,15 @@
 ﻿using System.Windows;
+using System;
+using System.Diagnostics;
+using System.Media;
+
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+
 
 namespace pi_number_game
 {
@@ -7,6 +18,8 @@ namespace pi_number_game
         public MainWindow()
         {
             InitializeComponent();
+            
         }
+        
     }
 }
--- pi-number-game/pi-number-game/Stopwatch.cs
+++ pi-number-game/pi-number-game/Stopwatch.cs
@@ -1,12 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Threading;
+
 
 namespace pi_number_game
 {
-    internal class Stopwatch
+    internal class StopwatchEvent
     {
+        private DispatcherTimer timer;
+        private Stopwatch stopWatch;
+        private string currentTime;
+
+        public StopwatchEvent()
+        {
+            timer = new DispatcherTimer();
+            stopWatch = new Stopwatch();
+            currentTime = string.Empty;
+        }
+        public void Run()
+        {
+            stopWatch.Start();
+            timer.Tick += new EventHandler(Tick);
+            timer.Start();
+        }
+        public void Stop()
+        {
+            if (stopWatch.IsRunning)
+            {
+                stopWatch.Stop();
+            }
+        }
+        public void Reset()
+        {
+            stopWatch.Reset();
+            //StopwatchBlock.Text = "00:00:00";
+        }
+        private void Tick(object sender, EventArgs e)
+        {
+            if (stopWatch.IsRunning)
+            {
+                TimeSpan ts = stopWatch.Elapsed;
+                currentTime = String.Format("{1:00}:{2:00}.{3:00}",
+                    ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+               // StopwatchBlock.Text = currentTime;
+
+            }
+        }
+
     }
 }
