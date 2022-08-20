ba1728f621aea9c9ade7a0d7a06846621ea19d86
Liza Snizhko <snizhko.liza@gmail.com>
2022-08-19 23:31:22 -0700

    Attemped creating function for only numbers input. Failed

--- pi-number-game/pi-number-game/MainWindow.xaml.cs
+++ pi-number-game/pi-number-game/MainWindow.xaml.cs
@@ -1,25 +1,30 @@
-﻿using System.Windows;
-using System;
-using System.Diagnostics;
-using System.Media;
-
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Threading;
-
-
+﻿using System.Text.RegularExpressions;
+using System.Windows;
 
 namespace pi_number_game
 {
     public partial class MainWindow : Window
     {
+        StopwatchEvent stopwatch = new StopwatchEvent();
+
         public MainWindow()
         {
             InitializeComponent();
-            
+
         }
-        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //stopwatch.Run();
+        }
+        private void PreviewOnlyNumbersDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            Regex _regex = new Regex("[^0-9.-]+");
+            if (_regex.IsMatch(FromTextBox.Text)) e.Handled = true;
+        }
+        public void InputNumsOnly(string s, System.Windows.Input.KeyEventArgs e)
+        {
+            Regex _regex = new Regex("[^0-9.-]+");
+            if (_regex.IsMatch(s)) e.Handled = true;
+        }
     }
 }
